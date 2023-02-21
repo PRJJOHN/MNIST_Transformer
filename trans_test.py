cfcf8428e80c8b780a67a0cb512bac742f7ca019
John Hsu <johnhsu@biologue.net>
2023-02-21 19:21:52 +0800

    Fix the issue fork func don't support in Windows

--- trans_test.py
+++ trans_test.py
@@ -3,26 +3,32 @@ import torchvision
 import torchvision.transforms as transforms
 N_EPHOCS = 30
 BATCH_SIZE = 128
-transform = transforms.Compose([transforms.ToTensor()])
-model= torch.load('mnist_trans.pt')
 def get_accuracy(logit, target, batch_size):
     ''' Obtain accuracy for training round '''
     corrects = (torch.max(logit, 1)[1].view(target.size()).data == target.data).sum()
     accuracy = 100.0 * corrects/batch_size
     return accuracy.item()
-testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE, shuffle=False, num_workers=2)
-test_acc = 0.0
-model = model.cpu()
-for i, data in enumerate(testloader, 0):
-    inputs, labels = data
-    #inputs = inputs.view(-1, 28, 28)
+def main():
+    transform = transforms.Compose([transforms.ToTensor()])
+    model= torch.load('mnist_trans.pt')
 
-#    zerofill = torch.zeros(inputs.size(0), 7, 28)
-#    inputs[:,0:7,:] = zerofill
-    #imshow(inputs[0,:,:])
-    outputs = model(inputs)
+    # trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+    # trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
+    testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE, shuffle=False, num_workers=1)
+    test_acc = 0.0
+    model = model.cpu()
+    for i, data in enumerate(testloader, 0):
+        inputs, labels = data
+        #inputs = inputs.view(-1, 28, 28)
 
-    test_acc += get_accuracy(outputs, labels, BATCH_SIZE)
-        
-print('Test Accuracy: %.2f'%( test_acc/i))
+    #    zerofill = torch.zeros(inputs.size(0), 7, 28)
+    #    inputs[:,0:7,:] = zerofill
+        #imshow(inputs[0,:,:])
+        outputs = model(inputs)
+
+        test_acc += get_accuracy(outputs, labels, BATCH_SIZE)
+            
+    print('Test Accuracy: %.2f'%( test_acc/i))
+if __name__ == '__main__':
+    main()
